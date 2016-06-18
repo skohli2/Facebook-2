65b073442e97f095621481f1495992d8fb3d8d01
skohli2 <skohli2@L-SNVN0NDF1G-M.homeoffice.wal-mart.com>
2016-06-17 19:42:02 -0700

    Added Black View between photo and News Feed controllers

--- Facebook/LightBoxTransition.swift
+++ Facebook/LightBoxTransition.swift
@@ -9,13 +9,24 @@
 import UIKit
 
 class LightBoxTransition: BaseTransition {
+    var blackView = UIView()
     override func presentTransition(containerView: UIView, fromViewController: UIViewController, toViewController: UIViewController) {
+//         let newsFeedController = fromViewController as! NewsFeedViewController
+//         let photoViewController = toViewController as! PhotoViewController
+        
+        
+        blackView.backgroundColor = UIColor(white: 0, alpha: 0.8)
+        blackView.frame.size = containerView.frame.size
+        blackView.alpha = 0
+        containerView.insertSubview(blackView, belowSubview: toViewController.view)
+        
         
         toViewController.view.alpha = 0
         toViewController.view.transform = CGAffineTransformMakeScale(0, 0)
         UIView.animateWithDuration(duration, animations: {
             toViewController.view.alpha = 1
-        toViewController.view.transform = CGAffineTransformMakeScale(1, 0.8)
+            toViewController.view.transform = CGAffineTransformMakeScale(1, 0.8)
+            self.blackView.alpha = 1
         }) { (finished: Bool) -> Void in
             self.finish()
         }
@@ -26,6 +37,7 @@ class LightBoxTransition: BaseTransition {
         fromViewController.view.alpha = 1
         fromViewController.view.transform = CGAffineTransformMakeScale(1, 0.8)
         UIView.animateWithDuration(duration, animations: {
+            self.blackView.alpha = 0
             fromViewController.view.alpha = 0
         fromViewController.view.transform = CGAffineTransformMakeScale(0.001, 0.001)
         }) { (finished: Bool) -> Void in
